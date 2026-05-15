cf34ea70d2c4bcf14a4e03c569f53a1d13c6e271
josuemoraisgh <josuemorais@gmail.com>
2026-05-15 14:44:32 -0300

    update esp32

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 
-// Teste minimo: pisca LED RGB no pino 48 (ESP32-S3-DevKitC-1)
-#define RGB_PIN 48
+// #define RGB_PIN 48 // Teste minimo: pisca LED RGB no pino 48 (ESP32-S3-DevKitC-1)
+#define RGB_PIN 13
 
 void setup()
 {
   Serial.begin(115200);
+  pinMode(RGB_PIN, OUTPUT);
   delay(2000);  // espera USB reconectar apos reset
   Serial.println("=== ESP32-S3 Teste Minimo ===");
 }
@@ -13,22 +14,32 @@ void setup()
 void loop()
 {
   // Vermelho
-  neopixelWrite(RGB_PIN, 255, 0, 0);
+  digitalWrite(RGB_PIN, HIGH);
   Serial.println("VERMELHO");
   delay(500);
 
   // Apagado
-  neopixelWrite(RGB_PIN, 0, 0, 0);
+  digitalWrite(RGB_PIN, LOW);
   Serial.println("APAGADO");
   delay(500);
 
-  // Verde
-  neopixelWrite(RGB_PIN, 0, 255, 0);
-  Serial.println("VERDE");
-  delay(500);
+  // // Vermelho
+  // neopixelWrite(RGB_PIN, 255, 0, 0);
+  // Serial.println("VERMELHO");
+  // delay(500);
 
-  // Apagado
-  neopixelWrite(RGB_PIN, 0, 0, 0);
-  Serial.println("APAGADO");
-  delay(500);
+  // // Apagado
+  // neopixelWrite(RGB_PIN, 0, 0, 0);
+  // Serial.println("APAGADO");
+  // delay(500);
+
+  // // Verde
+  // neopixelWrite(RGB_PIN, 0, 255, 0);
+  // Serial.println("VERDE");
+  // delay(500);
+
+  // // Apagado
+  // neopixelWrite(RGB_PIN, 0, 0, 0);
+  // Serial.println("APAGADO");
+  // delay(500);
 }
